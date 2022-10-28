4eb528f7bffb60985beb0f30d7e6d54571de7ccc
4Sight <116753476+4S1ght@users.noreply.github.com>
2022-10-28 13:50:57 +0200

    Added EventProxy and Process class.

---
+++ src/class/EventProxy.class.ts
@@ -0,0 +1,42 @@
+
+import EventEmitter from "events";
+
+/**
+ * EventProxy is a class built on top of EventEmitter that adds pause/resume functionality.
+ * This is useful in cases where a child process was killed manually from the CLI
+ * but there's no intention to trigger an event indicating its unexpected death.
+ */
+export default class EventProxy<Event extends string = any> extends EventEmitter {
+
+    public pausedEvts: Record<string, boolean> = {}
+
+    constructor() {
+        super()
+    }
+
+    /** 
+     * Synchronously calls each of the listeners registered for the event if it's not paused. 
+     */
+    public emitCustom(event: Event, ...args: any[]) {
+        if (this.pausedEvts[event]) return this
+        this.emit(event, args)
+        return this
+    }
+
+    /** 
+     * Blocks the proxy from emitting a specific event. 
+     */
+    public pause(event: Event) {
+        this.pausedEvts[event] = true
+        return this
+    }
+
+    /** 
+     * Resumes emitting of an event previously blocked by `Proxy.pause()` 
+     */
+    public resume(event: Event) {
+        this.pausedEvts[event] = false
+        return this
+    }
+
+}
---
+++ src/class/Process.class.ts
@@ -0,0 +1,125 @@
+
+import EventEmitter from "events";
+import EventProxy from "./EventProxy.class";
+import cp from 'child_process';
+import treeKill from "tree-kill";
+
+
+export default interface Process {
+    on(event: 'spawn',          callback: () => any): this
+    on(event: 'close',          callback: () => any): this
+    on(event: 'kill',           callback: () => any): this
+    on(event: 'restart',        callback: () => any): this
+    on(event: 'kill-error',     callback: (err: Error) => any): this
+    on(event: 'restart-error',  callback: (err: Error) => any): this
+}
+export default class Process extends EventProxy<'spawn' | 'close' | 'kill' | 'restart' | 'kill-error' | 'restart-error'> {
+
+    declare public child: cp.ChildProcess
+    declare public alive: boolean
+
+    public spawnCommand: string;
+    public spawnArguments: string[];
+    public spawnOptions: cp.SpawnOptions;
+
+    public status: 'awaiting' | 'alive' | 'dead' | 'killed';
+    public restarted = false;
+
+    constructor(command: string, argv: string[], options?: cp.SpawnOptions) {
+
+        super()
+
+        this.spawnCommand = command;
+        this.spawnArguments = argv;
+        this.spawnOptions = options || {};
+
+        this.status = 'awaiting';
+
+    }
+
+    private registerEvents() {
+
+        let self = this;
+
+        this.child.on("spawn", () => {
+            self.alive = true;
+            self.status = "alive";
+            self.emitCustom("spawn");
+            // Resume "close" because it might have been paused by kill()
+            self.resume("close");
+        });
+ 
+        this.child.on('exit', () => {
+            self.alive = false;
+            if (self.status !== "killed") self.status = "dead";
+            // "close" doesn't fire if process was killed manually
+            self.emitCustom("close");
+            self.pause('close');
+        });
+
+        return this;
+
+    }
+
+    public spawn = () => {
+
+        this.child = cp.spawn(this.spawnCommand, this.spawnArguments, {
+            stdio: ['ignore', 'inherit', 'inherit'],
+            shell: true,
+            ...this.spawnOptions
+        });
+        this.registerEvents();
+        return this;
+
+    }
+
+    public kill = (silent = false) => new Promise<void>((resolve, reject) => {
+        
+        this.pause('close')
+
+        treeKill(this.child.pid!, (error) => {
+            if (error) {
+                reject(error);
+                this.emit('kill-error', error);
+                this.resume('close');
+            }
+            else {
+                this.status = 'killed';
+                if (!silent) this.emitCustom('kill');
+                resolve();
+            }
+        });
+
+    });
+
+    public restart = () => new Promise<void>((resolve, reject) => {
+
+        this.pause('close');
+        this.restarted = true;
+
+        treeKill(this.child.pid!, (error) => {
+            if (error) {
+                reject(error);
+                this.emitCustom('restart-error', error);
+                this.resume('close');
+            }
+            else {
+                this.spawn();
+                this.registerEvents();
+                resolve();
+                this.emitCustom('restart');
+            }
+        })       
+
+    });
+
+    public revive = () => {
+        if (this.status === 'alive' || this.status === 'awaiting') return this;
+        this.restarted = true;
+        this.spawn();
+        return this;
+    }
+
+
+}
+
